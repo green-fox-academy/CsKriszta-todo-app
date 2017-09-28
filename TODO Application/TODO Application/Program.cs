089e0eb2d0989c1417b3c99779ee3ade0bf2b313
CsKriszta93 <kriszta.csere93@gmail.com>
2017-09-28 15:04:09 +0200

    ReadFile function created

--- TODO Application/TODO Application/Program.cs
+++ TODO Application/TODO Application/Program.cs
@@ -3,22 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace TODO_Application
 {
     class Program
     {
-        public void PrintUsage(params string[] args)
+        public static void PrintUsage(string[] args)
         {
-            string usage = "Command line todo application \n ============================= \n \n \n Command line arguments \n -a  Adds a new task \n -r  Removes an task \n -c  Completes an task";
+            string usage = "Command line todo application \n============================= \n \n \n Command line arguments: \n -l  Lists all the tasks \n -a  Adds a new task \n -r  Removes an task \n -c  Completes an task";
             if (args.Length == 0)
             {
                 Console.WriteLine(usage); 
             } 
-            Console.ReadLine();
+        }
+
+        public static List<string> ReadList()
+        {
+            StreamReader TDList = new StreamReader("TextList.txt");
+            string line = "";
+            var list = new List<string>();
+            while (line != null)
+            {
+                line = TDList.ReadLine();
+                list.Add(line);
+            }
+            TDList.Close();
+            return list;           
+        }
+
+        public static void PrintList(List<string> inputList)
+        {
+            for(int i = 0; i < inputList.Count; i++)
+            {
+                Console.WriteLine(i.ToString() + inputList[i]);
+            }
         }
         static void Main(string[] args)
         {
+            PrintUsage(args);
+            Console.ReadLine();
+
+            if (args ==
 
         }
     }
