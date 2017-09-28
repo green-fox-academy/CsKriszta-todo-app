b4f88f6c976f0202d604ab088a0ec3d0102cd524
CsKriszta93 <kriszta.csere93@gmail.com>
2017-09-28 12:07:55 +0200

    Print Usage function created

---
+++ TODO Application/TODO Application/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TODO_Application
+{
+    class Program
+    {
+        public void PrintUsage(params string[] args)
+        {
+            string usage = "Command line todo application \n ============================= \n \n \n Command line arguments \n -a  Adds a new task \n -r  Removes an task \n -c  Completes an task";
+            if (args.Length == 0)
+            {
+                Console.WriteLine(usage); 
+            } 
+            Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
